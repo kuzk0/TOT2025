6600834b879ea29bfdd21ef81971e7f3adfab8a0
Anastasia <AnamiGhost@yandex.ru>
2024-04-11 01:01:52 +0300

    Changed

--- Assets/New Folder/Info.cs
+++ Assets/New Folder/Info.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Info : MonoBehaviour
 {
     public float number;
+    private float GetFloat(string stringValue, float defaultValue)
+    {
+        float result = defaultValue;
+        float.TryParse(stringValue, out result);
+        return result;
+    }
+    public void ButClick(GameObject Input)
+    {
+        number = GetFloat(Input.GetComponent<TextMeshProUGUI>().text,0.0f);
+        Debug.Log(Input.GetComponent<TextMeshProUGUI>().text);
+    }
 }
